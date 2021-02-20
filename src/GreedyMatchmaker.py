a503cf467edfa64c42a31f710df3ab7a35f89c44
n_razdobreev <n_razdobreev@wargaming.net>
2021-02-20 10:58:23 +0300

    Added check for full team in greedyMatchmaker

---
+++ src/GreedyMatchmaker.py
@@ -0,0 +1,34 @@
+from battleGroup import BattleGroup, Team
+
+
+class GreedMatchmaker:
+	def __init__(self, team_size, teams_num):
+		self.__queue = []
+		self.__team_size = team_size
+		self.__teams_num = teams_num
+
+	def enqueueDivision(self, division):
+		self.__queue.append(division)
+
+	def dequeueDivision(self, division):
+		self.__queue.remove(division)
+
+	def process(self):
+		battle_group = BattleGroup(Team() for _ in range(self.__teams_num))
+		queue = list(self.__queue)
+		division = queue.pop(0)
+		battle_group = BattleGroup.addDivision(battle_group, 0, division)
+		place_left = self.__team_size * self.__teams_num * sum(team.size for team in battle_group.teams)
+		while place_left and queue:
+			for team_id, team in enumerate(battle_group.teams):
+				if team.size < self.__team_size:
+					for division in queue:
+						if division.size <= self.__team_size - team.size:
+							battle_group = BattleGroup.addDivision(battle_group, team_id, division)
+							queue.remove(division)
+							place_left -= division
+							break
+		if place_left > 0:
+			return None
+
+		return battle_group
