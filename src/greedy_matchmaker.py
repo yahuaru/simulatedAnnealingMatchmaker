f7ad199fde426ccde9824bd8859d54ef97631378
Nikita Razdobreev <nikita.razdobreev@protonmail.com>
2021-02-27 16:30:16 +0300

    Changed greedy_matchmaker algorithm

--- src/greedy_matchmaker.py
+++ src/greedy_matchmaker.py
@@ -18,22 +18,23 @@ class GreedyMatchmaker:
 	def process(self):
 		battle_group = BattleGroup(Team() for _ in range(self.__teams_num))
 		queue = list(self.__queue)
-		division = queue.pop(random.randint(0, len(queue) - 1))
-		battle_group = BattleGroup.addDivision(battle_group, 0, division)
-		for team_id in range(self.__teams_num):
-			team_size = battle_group.teams[team_id].size
-			current_queue = list(queue)
-			for division in current_queue:
-				if division.size <= self.__team_size - team_size:
-					battle_group = BattleGroup.addDivision(battle_group, team_id, division)
-					queue.remove(division)
-					team_size = battle_group.teams[team_id].size
-					if team_size == self.__team_size:
-						break
+		while queue:
+			division = queue.pop(random.randint(0, len(queue) - 1))
+			battle_group = BattleGroup.addDivision(battle_group, 0, division)
+			for team_id in range(self.__teams_num):
+				team_size = battle_group.teams[team_id].size
+				current_queue = list(queue)
+				for division in current_queue:
+					if division.size <= self.__team_size - team_size:
+						battle_group = BattleGroup.addDivision(battle_group, team_id, division)
+						queue.remove(division)
+						team_size = battle_group.teams[team_id].size
+						if team_size == self.__team_size:
+							break
 
-		if all(team.size == self.__team_size for team in battle_group.teams):
-			for team in battle_group.teams:
-				for division in team.divisions:
-					self.__queue.remove(division)
-			return battle_group
+			if all(team.size == self.__team_size for team in battle_group.teams):
+				for team in battle_group.teams:
+					for division in team.divisions:
+						self.__queue.remove(division)
+				return battle_group
 		return None
